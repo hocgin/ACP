a7225081a8d8c3fb2797561bcc8f1c572ec89690
hocgin <hocgin@gmail.com>
2016-12-15 21:34:17 +0800

    add recommend article task

---
+++ src/in/hocg/acp/tasks/RecommendTask.java
@@ -0,0 +1,22 @@
+package in.hocg.acp.tasks;
+
+import in.hocg.acp.bean.Result;
+import in.hocg.acp.bean.articles.Article;
+
+import java.util.List;
+
+/**
+ * Created by hocgin on 16-12-15.
+ */
+public class RecommendTask extends BaseTask {
+	@Override
+	public Result.Classify getClassify() {
+		return Result.Classify.Recommend;
+	}
+	
+	@Override
+	public List<Article> exec() {
+		// 推荐文章模板
+		return null;
+	}
+}
